d97190542aa3d3c60b307c5443d50a8fb59d8f94
ItsDresch <65150772+swolem12@users.noreply.github.com>
2025-12-23 10:53:49 +0000

    Bump SW to v105

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for AFOQT Study Console
 // Provides offline caching and PWA functionality
 
-const CACHE_NAME = 'afoqt-quest-v104';
+const CACHE_NAME = 'afoqt-quest-v105';
 const urlsToCache = [
   './',
   './index.html',
